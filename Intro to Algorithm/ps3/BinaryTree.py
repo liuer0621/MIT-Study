bfeb6e5b278229adfccceda14f8981d66f40a936
liuer0621 <lanachensun@gmail.com>
2014-12-23 22:30:12 -0800

    successfully print tree

--- Intro to Algorithm/ps3/BinaryTree.py
+++ Intro to Algorithm/ps3/BinaryTree.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+import math
+
 
 class Node():
 	def __init__(self,value):
@@ -52,6 +54,7 @@ class BinaryTree():
 			if root.getLeft() == None :
 				root.insertLeft(newNode)
 				newNode.setHeight(currentHeight+1)
+				newNode.setParent(root)
 				if self.maxHeight < currentHeight+1 :
 					self.maxHeight = currentHeight+1
 			else : 
@@ -61,6 +64,7 @@ class BinaryTree():
 			if root.getRight() == None :
 				root.insertRight(newNode)
 				newNode.setHeight(currentHeight+1)
+				newNode.setParent(root)
 				if self.maxHeight < currentHeight+1 :
 					self.maxHeight = currentHeight+1
 
@@ -74,16 +78,38 @@ class BinaryTree():
 			pass
 		else :
 			self.printTree(root.getLeft())
-			print root.getValue(),root.getHeight()
+			print root.getValue()
 			self.printTree(root.getRight())	
 
+
+	def printLines(self,blankNo,current_level):
+		out = []
+	
+		for j in range(2**(current_level-1)) :
+			for i in range(int(math.floor(blankNo))+1):
+				out.append(' ')
+
+			out.append('/')
+			for i in range(int(math.ceil(blankNo))):
+				out.append(' ')
+
+			for i in range(int(math.ceil(blankNo))):
+				out.append(' ')
+
+			out.append('\\')
+	
+			for i in range(int(math.ceil(blankNo))):
+				out.append(' ')
+
+
+		return "".join(out)
 
 	def printGraph(self):
 		queue = [self.root]
 		out = []
 		current_level = self.root.getHeight()
-		zeroNode = Node(-1)
-		zeroNode.setHeight(-1)
+		#zeroNode = Node(-1)
+		#zeroNode.setHeight(-1)
 		cellWidth = 2
 		maxWidth = (cellWidth + 1) * 2**self.maxHeight
 		blankNo = int((maxWidth/(2**0) - cellWidth) / 2)
@@ -91,46 +117,52 @@ class BinaryTree():
 		while len(queue) > 0 :
 			
 			current_node = queue.pop(0)
-#			import pdb;pdb.set_trace()
-			
+			#import pdb;pdb.set_trace()
 			if current_node.getHeight() > current_level :
 				current_level += 1
-                                blankNo = int((maxWidth/(2**current_level) - cellWidth) / 2)
+                                blankNo = (maxWidth/(2**current_level) - cellWidth) / 2.0
 				out.append("\n")
-				for i in range(blankNo):
+				outLines = self.printLines(blankNo,current_level)
+				out.append(outLines)
+				out.append("\n")
+				for i in range(int(math.floor(blankNo))):
 					out.append(' ')
 			
 			else :
-				for i in range(blankNo) :
+				for i in range(int(math.floor(blankNo))) :
 					out.append(' ')
 
 			if current_node.getValue() == -1 :
-				out.append(' ')
+				out.append('  ')
 			else :
 				out.append(str(current_node.getValue()))
-			for i in range(blankNo) :
+			for i in range(int(math.ceil(blankNo))) :
 				out.append(' ')
 
 			if current_node.getLeft() is not None :
 				queue.append(current_node.getLeft())
-			elif current_node is not zeroNode :
+			elif current_node.getHeight() < self.maxHeight :
+				zeroNode = Node(-1)
+				zeroNode.setHeight(current_node.getHeight()+1)
+				zeroNode.setParent(current_node)
 				queue.append(zeroNode)
 
 			if current_node.getRight() is not None :
 				queue.append(current_node.getRight())
 
-			elif current_node is not zeroNode :
+			elif current_node.getHeight() < self.maxHeight:
+				zeroNode = Node(-1)
+				zeroNode.setHeight(current_node.getHeight()+1)
+				zeroNode.setParent(current_node)
 				queue.append(zeroNode)
 
-		output = "".join(out).split("\n")
-		print output
 		print "".join(out)
 		
 						
 			
 
 
-input = [70,31,93,14,73,94,23]
+input = [70,31,93,14,73,94,23,39,90,83,53]
 
 tree = BinaryTree(Node(input[0]))
 
